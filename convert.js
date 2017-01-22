a7f212dde878c1a7a2b80a90d96edc1df285db40
jiahao <15029218541@163.com>
2017-01-22 10:31:28 +0800

    some changes

---
+++ convert.js
@@ -0,0 +1,63 @@
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
+
+function getPostList() {
+  var path = __dirname + "/src/posts.json";
+  return fs.readFileAsync(path);
+}
+
+function newPostList(content) {
+  var dir = __dirname + '/build';
+  if(!fs.existsSync(dir)) fs.mkdirSync(dir);
+  var path = __dirname + "/build/index.md";
+  return fs.writeFileAsync(path, content);
+}
+
+function getPost(index) {
+  var postPath = __dirname + `/src/posts/${index}.md`;
+  return fs.readFileAsync(postPath);
+}
+
+function cardTemp(item) {
+  return `
+  <a class="item" href="/v/${item.id}.html">
+    <span class="left">${item.id}</span>
+    <span class="right">
+      <span class="title">${item.title}</span>
+      <span class="date">${item.created_at}</span>
+    </span>
+  </a>`
+}
+
+function slogan(item) {
+  return `<div class="slogan">
+  <div class="title">${item.title}</div>
+  <div class="date">${item.created_at}</div>
+</div>\n`
+}
+
+function video(item) {
+  return `<div class="video-wrapper">
+  <video controls>
+    <source src="http://7xnm4l.com1.z0.glb.clouddn.com/${item.name}.mp4" type="video/mp4" />
+  </video>
+</div>\n\n`
+}
+
+getPostList().then(function(list) {
+  var arr = JSON.parse(list);
+  var cards = arr.map(function(item, i) {
+    return cardTemp(item);
+  });
+  newPostList(cards.reverse().join('\n'));
+  var dir = __dirname + '/build/v';
+  if(!fs.existsSync(dir)) fs.mkdirSync(dir);
+  arr.forEach(function(item, i) {
+    var str = slogan(item);
+    var media = video(item);
+    var postPath = __dirname + `/build/v/${i+1}.md`;
+    getPost(i+1).then(function(content) {
+      fs.writeFileAsync(postPath, str + media + content);
+    })
+  })
+})
